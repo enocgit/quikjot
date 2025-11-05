859d4404bd16e8907c6cf7431fa94db6642d9007
Enoch Ansah <enocansah@gamil.com>
2025-11-05 13:10:43 +0000

    Fix responsiveness across several sections and layouts

--- components/header.tsx
+++ components/header.tsx
@@ -1,23 +1,36 @@
 "use client";
 
 import React from "react";
-import { SearchIcon } from "lucide-react";
+import { MenuIcon, SearchIcon } from "lucide-react";
 import { Typography } from "./ui/typography";
 import Menu from "./ui/menu";
 import dynamic from "next/dynamic";
 import { InputGroup, InputGroupAddon, InputGroupInput } from "./ui/input.group";
 import { Kbd } from "./ui/kbd";
+import { SidebarTrigger, useSidebar } from "./ui/sidebar";
+import { Button } from "./ui/button";
 const ModeToggle = dynamic(() => import("./ui/mode-toggle"), {
   ssr: false,
 });
 
 const Header: React.FC = () => {
+  const { toggleSidebar } = useSidebar();
   return (
-    <header className="bg-background flex h-fit w-full justify-between gap-5 px-6 py-4 pt-8">
-      <Typography variant="h3" className="font-semibold">
-        My Notes
-      </Typography>
-      <div className="bg-muted/50 flex w-full max-w-md flex-col gap-6">
+    <header className="bg-background @container/header flex h-fit w-full justify-between gap-5 px-6 py-4 pt-8">
+      <div className="flex items-center gap-2">
+        <Button
+          variant="outline"
+          size="icon"
+          onClick={toggleSidebar}
+          className="md:hidden"
+        >
+          <MenuIcon />
+        </Button>
+        <Typography variant="h3" className="font-semibold">
+          My Notes
+        </Typography>
+      </div>
+      <div className="bg-muted/50 hidden w-full max-w-md flex-col gap-6 @3xl:flex">
         <InputGroup>
           <InputGroupInput placeholder="Search..." />
           <InputGroupAddon>
@@ -29,20 +42,11 @@ const Header: React.FC = () => {
           </InputGroupAddon>
         </InputGroup>
       </div>
-      {/* <div className="flex flex-1 justify-center">
-        <div className="relative w-full max-w-md">
-          <Input
-            type="text"
-            placeholder="Search notes..."
-            className="bg-muted/50 pl-10"
-          />
-          <span className="text-muted-foreground absolute top-1/2 left-3 -translate-y-1/2">
-            <Search size={18} />
-          </span>
-        </div>
-      </div> */}
 
       <div className="flex items-center gap-4">
+        <Button variant="ghost" size="icon" className="@3xl:hidden">
+          <SearchIcon />
+        </Button>
         {/* Dark/Light Mode Toggle */}
         <ModeToggle />
         {/* Avatar with Dropdown Menu */}
